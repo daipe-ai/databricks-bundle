e823a890e58f42092a573f12c0a268869a5d534d
Jiri Koutny <jirkakoutny@gmail.com>
2020-02-16 19:02:38 +0100

    Notebook error handler auto-registration
    When bundle is loaded into Pyfony

--- src/databricksbundle/DatabricksBundle.py
+++ src/databricksbundle/DatabricksBundle.py
@@ -1,4 +1,10 @@
+from injecta.container.ContainerInterface import ContainerInterface
 from pyfonybundles.Bundle import Bundle
+from databricksbundle.notebook.NotebookErrorHandler import setNotebookErrorHandler
+from databricksbundle.detector import isDatabricks
 
 class DatabricksBundle(Bundle):
-    pass
+
+    def boot(self, container: ContainerInterface):
+        if isDatabricks() and container.getConfig().databricksbundle.enableNotebookErrorHandler is True:
+            setNotebookErrorHandler()
