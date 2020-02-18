610034137414f2398fdd90e4e8bc9589f4cecc31
Jiri Koutny <jirkakoutny@gmail.com>
2020-02-18 21:10:41 +0100

    Disable handler or notebooks stored in /Users dir

--- src/databricksbundle/DatabricksBundle.py
+++ src/databricksbundle/DatabricksBundle.py
@@ -1,10 +1,18 @@
+import re
 from injecta.container.ContainerInterface import ContainerInterface
 from pyfonybundles.Bundle import Bundle
 from databricksbundle.notebook.NotebookErrorHandler import setNotebookErrorHandler
 from databricksbundle.detector import isDatabricks
+from databricksbundle.notebook.helpers import getNotebookPath
 
 class DatabricksBundle(Bundle):
 
     def boot(self, container: ContainerInterface):
-        if isDatabricks() and container.getConfig().databricksbundle.enableNotebookErrorHandler is True:
+        parameters = container.getConfig()
+
+        if (
+            isDatabricks()
+            and parameters.databricksbundle.enableNotebookErrorHandler is True
+            and not re.match('^/Users/', getNotebookPath())
+        ):
             setNotebookErrorHandler()
