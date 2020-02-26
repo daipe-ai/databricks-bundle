49ed3497f68aab3f8072fa931893c1966193930b
Jiri Koutny <jirkakoutny@gmail.com>
2020-02-26 22:01:43 +0100

    container test added
    To be able to test if all container services are wired properly

---
+++ src/databricksbundle/DatabricksBundleTest.py
@@ -0,0 +1,33 @@
+import unittest
+from injecta.testing.servicesTester import testServices
+from injecta.config.YamlConfigReader import YamlConfigReader
+from injecta.package.pathResolver import resolvePath
+from typing import List
+from pyfony.PyfonyBundle import PyfonyBundle
+from pyfony.kernel.BaseKernel import BaseKernel
+from pyfonybundles.Bundle import Bundle
+from databricksbundle.DatabricksBundle import DatabricksBundle
+
+class DatabricksBundleTest(unittest.TestCase):
+
+    def test_init(self):
+        class Kernel(BaseKernel):
+
+            def _registerBundles(self) -> List[Bundle]:
+                return [
+                    PyfonyBundle(),
+                    DatabricksBundle()
+                ]
+
+        kernel = Kernel(
+            'test',
+            resolvePath('databricksbundle') + '/DatabricksBundleTest',
+            YamlConfigReader()
+        )
+
+        container = kernel.initContainer()
+
+        testServices(container)
+
+if __name__ == '__main__':
+    unittest.main()
