37c9e0c92462ae8fd17131799321cba015b22b6d
Roman Novosad <roman.novosad@datasentics.com>
2020-11-03 01:49:34 +0100

    modify detector of databricks cluster

--- src/databricksbundle/detector.py
+++ src/databricksbundle/detector.py
@@ -1,4 +1,11 @@
-from importlib.util import find_spec
+import os
+
 
 def isDatabricks():
-    return find_spec('pyspark.dbutils') is None
+    """
+    Check, if scripts are running on databricks cluster or locally.
+
+    DATABRICKS_RUNTIME_VERSION available for all version and only on databricks cluster
+    :return: true, if running on databricks
+    """
+    return os.getenv("DATABRICKS_RUNTIME_VERSION") is not None
