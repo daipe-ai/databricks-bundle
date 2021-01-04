f5851ba7b512ef110506e94cf5514a9a9ac46527
Jiri Koutny <jirkakoutny@gmail.com>
2021-01-04 20:19:12 +0100

    pylint supports class constants
    FOO_BAR

--- pylint.sh
+++ pylint.sh
@@ -10,6 +10,6 @@ pylint \
 --attr-rgx="^[_]{0,2}[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?$" \
 --argument-rgx="^[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?$" \
 --variable-rgx="^[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?$" \
---class-attribute-rgx="^[_]{0,2}[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?$" \
+--class-attribute-rgx="^([_]{0,2}[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?|[A-Z]+(_[A-Z0-9]+)+)$" \
 --const-rgx="^[a-z]+((\d)|([A-Z0-9][a-z0-9]+))*([A-Z])?$" \
 src
