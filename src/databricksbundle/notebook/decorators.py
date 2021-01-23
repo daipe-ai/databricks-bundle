b442ace97363132965528af19b8813aa85ec9296
Jiri Koutny <jirkakoutny@gmail.com>
2021-01-23 16:45:39 +0100

    tableParams must be part of datalakebundle

--- src/databricksbundle/notebook/decorators.py
+++ src/databricksbundle/notebook/decorators.py
@@ -1,5 +1,4 @@
 # pylint: disable = unused-import
-from databricksbundle.notebook.decorator.tableParams import tableParams
 from databricksbundle.notebook.decorator.notebookFunction import notebookFunction
 from databricksbundle.etl.dataFrameLoader import dataFrameLoader
 from databricksbundle.etl.transformation import transformation
