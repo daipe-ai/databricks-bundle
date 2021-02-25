8abd7681426c04f67b29804ad1f332640369921c
Marek Chlubna <marek.chlubna@datasentics.com>
2021-02-25 10:00:35 +0100

    databricks-connect test command added (dbx:test-connection)

---
+++ src/databricksbundle/test/DatabricksConnectTestCommand.py
@@ -0,0 +1,30 @@
+from logging import Logger
+from pyspark.sql import SparkSession
+from argparse import Namespace
+from consolebundle.ConsoleCommand import ConsoleCommand
+
+
+class DatabricksConnectTestCommand(ConsoleCommand):
+
+    def __init__(
+        self,
+        logger: Logger,
+        spark: SparkSession
+    ):
+        self.__logger = logger
+        self.__spark = spark
+
+    def getCommand(self) -> str:
+        return 'dbx:test-connection'
+
+    def getDescription(self):
+        return 'Test databricks-connect connection to Databricks instance'
+
+    def run(self, inputArgs: Namespace):
+        self.__logger.info('Testing the connectivity')
+        a = [1, 2, 3, 4]
+        b = [2, 3, 4, 8]
+        df = self.__spark.createDataFrame([a, b], schema=['a', 'b'])
+        self.__logger.info('Creating sample DataFrame')
+        df.show()
+        self.__logger.info('Connection successful')
