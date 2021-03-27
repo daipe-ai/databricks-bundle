d068bbb25730fb9a4596db8b27ecb1572d5f7775
Jiri Koutny <jirkakoutny@gmail.com>
2021-03-27 23:54:38 +0100

    Decorator parentheses test fixed
    (cherry picked from commit 5ffb3d118ae9f19e6b4719611e0a8aeb3d79b8e3)

--- src/databricksbundle/notebook/decorator/tests/notebook_function2_test.py
+++ src/databricksbundle/notebook/decorator/tests/notebook_function2_test.py
@@ -6,6 +6,7 @@ try:
     def load_data():
         return 155
 
+    raise Exception("Notebook function without parentheses must fail")
 
 except Exception as e:
     assert str(e) == "Use @notebook_function() instead of @notebook_function please"
