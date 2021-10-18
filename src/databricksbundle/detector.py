91aa9be6c80669634e6ccadd88c3021e5ac87fb4
Karel Gudera <karel.gudera@datasentics.com>
2021-10-18 18:47:38 +0200

    Detect if we are using Databricks Repos

--- src/databricksbundle/detector.py
+++ src/databricksbundle/detector.py
@@ -9,3 +9,7 @@ def is_databricks():
     :return: true, if running on databricks
     """
     return os.getenv("DATABRICKS_RUNTIME_VERSION") is not None
+
+
+def is_databricks_repo():
+    return is_databricks() and os.getcwd().startswith("/Workspace/Repos")
