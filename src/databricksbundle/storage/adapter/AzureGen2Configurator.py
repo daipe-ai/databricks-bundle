e13a6876ebd3a10d79cbe7768d5e1f2393bbe7d8
kutny <jirkakoutny@gmail.com>
2022-01-16 06:51:09 +0100

    DBUtils must be initialized manually
    To prevent issues with Databricks Connect initialization

--- src/databricksbundle/storage/adapter/AzureGen2Configurator.py
+++ src/databricksbundle/storage/adapter/AzureGen2Configurator.py
@@ -5,10 +5,10 @@ from databricksbundle.storage.StorageConfiguratorInterface import StorageConfigu
 
 
 class AzureGen2Configurator(StorageConfiguratorInterface):
-    def __init__(self, dbutils: DBUtils):
-        self.__dbutils = dbutils
-
     def configure(self, spark: SparkSession, config: Box):
+        # initialized manually to prevent issues with Databricks Connect initialization
+        dbutils = DBUtils(spark)
+
         spark.conf.set(
             f"fs.azure.account.oauth2.client.endpoint.{config.storage_name}.dfs.core.windows.net",
             f"https://login.microsoftonline.com/{config.tenant_id}/oauth2/token",
@@ -20,11 +20,11 @@ class AzureGen2Configurator(StorageConfiguratorInterface):
         spark.conf.set(f"fs.azure.account.auth.type.{config.storage_name}.dfs.core.windows.net", "OAuth")
         spark.conf.set(
             f"fs.azure.account.oauth2.client.id.{config.storage_name}.dfs.core.windows.net",
-            self.__dbutils.secrets.get(scope=config.client_id.secret_scope, key=config.client_id.secret_key),
+            dbutils.secrets.get(scope=config.client_id.secret_scope, key=config.client_id.secret_key),
         )
         spark.conf.set(
             f"fs.azure.account.oauth2.client.secret.{config.storage_name}.dfs.core.windows.net",
-            self.__dbutils.secrets.get(scope=config.client_secret.secret_scope, key=config.client_secret.secret_key),
+            dbutils.secrets.get(scope=config.client_secret.secret_scope, key=config.client_secret.secret_key),
         )
 
     def get_type(self):
