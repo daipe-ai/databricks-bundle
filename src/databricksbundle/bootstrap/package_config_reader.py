ba3a08007a910e9c7b2759ee3eaa0bc3bcf522a6
Karel Gudera <karel.gudera@datasentics.com>
2022-07-26 17:47:29 +0200

    Update to python3.8

--- src/databricksbundle/bootstrap/package_config_reader.py
+++ src/databricksbundle/bootstrap/package_config_reader.py
@@ -1,16 +1,9 @@
 import json
-import sys
 from pyfonycore.bootstrap.config import config_factory as pyfony_config_factory
 from pyfonycore.bootstrap.config.Config import Config
 from databricksbundle.detector import is_databricks_workspace
-
-if sys.version_info >= (3, 8):
-    from importlib import metadata as importlib_metadata
-else:
-    import importlib_metadata
-
-# pylint: disable=wrong-import-position
-from importlib_metadata import files
+from importlib import metadata as importlib_metadata
+from importlib.metadata import files
 
 
 def read() -> Config:
@@ -29,11 +22,8 @@ def read_config_from_master_package() -> Config:
     if not entry_points:
         raise Exception("pyfony.bootstrap entry points is missing in the master package, try rebuilding the package")
 
-    if hasattr(tuple(entry_points)[0], "dist"):
-        dist_path = tuple(entry_points)[0].dist
-    else:
-        package_name = entry_points.value
-        dist_path = files(package_name)[0].dist
+    package_name = entry_points[0].value
+    dist_path = files(package_name)[0].dist  # pyre-ignore[16]
 
     raw_config = json.loads(dist_path.read_text("bootstrap_config.json"))
 
